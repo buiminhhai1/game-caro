6ef091d30793842a42a078494b02d3d5ec174a2d
buiminhhai1 <ngovietduc20088@gmail.com>
2019-09-15 23:00:32 +0700

    Updates

---
+++ precache-manifest.aa85936689369fd5c76e97d62f53c4f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2482d9a346f6e3784aadd601f6a2e533",
+    "url": "/buiminhhai1/game-caro.git/index.html"
+  },
+  {
+    "revision": "5077333787d412c91a71",
+    "url": "/buiminhhai1/game-caro.git/static/css/main.ddd880af.chunk.css"
+  },
+  {
+    "revision": "615312fe6d460b3ed908",
+    "url": "/buiminhhai1/game-caro.git/static/js/2.20404cbd.chunk.js"
+  },
+  {
+    "revision": "5077333787d412c91a71",
+    "url": "/buiminhhai1/game-caro.git/static/js/main.21393978.chunk.js"
+  },
+  {
+    "revision": "928521b05e47392097e7",
+    "url": "/buiminhhai1/game-caro.git/static/js/runtime~main.347ab106.js"
+  },
+  {
+    "revision": "7fc49bd3abe10e8f32b8cfa5521289ce",
+    "url": "/buiminhhai1/game-caro.git/static/media/background.7fc49bd3.mp3"
+  },
+  {
+    "revision": "9b9275cae525a387e608a95ca8bed98d",
+    "url": "/buiminhhai1/game-caro.git/static/media/sound-win.9b9275ca.wav"
+  },
+  {
+    "revision": "25a9a88c9217fdbd8e7d31fe036e9f07",
+    "url": "/buiminhhai1/game-caro.git/static/media/toe.25a9a88c.wav"
+  }
+]);
